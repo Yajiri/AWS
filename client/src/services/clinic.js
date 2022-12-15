dd62384af44fa7213af8ba88e353817d6a28a8e0
juliavankirk <juliavankirk@gmail.com>
2022-12-15 10:14:47 +0100

    integrate clinic endpoints that have been elicited from the API specification for our use case

---
+++ client/src/services/clinic.js
@@ -0,0 +1,10 @@
+import { Api } from '@services/Api'
+
+export const clinicApi = {
+  getAllClinics: () => {
+    Api.get(`/clinics`)
+  },
+  getClinic: clinicId => {
+    Api.get(`clinics/${clinicId}`)
+  }
+}
