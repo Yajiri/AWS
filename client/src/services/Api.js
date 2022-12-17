2752ddeda14749c0fe6eed21367f1f6af5b51c51
juliavankirk <juliavankirk@gmail.com>
2022-12-17 15:07:33 +0100

    change to api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const Api = axios.create({
-  baseURL: 'https://8xob0txpdb.execute-api.eu-north-1.amazonaws.com/default'
+  baseURL: 'https://bmuk2llzl8.execute-api.eu-central-1.amazonaws.com/default'
 });
 
 export default (endpoint) => {
   return axios.create({
-    baseURL: 'https://8xob0txpdb.execute-api.eu-north-1.amazonaws.com/default'
+    baseURL: 'https://bmuk2llzl8.execute-api.eu-central-1.amazonaws.com/default'
   })
 }
--- client/src/services/test.js
+++ client/src/services/test.js
@@ -2,6 +2,6 @@ import { Api } from '../services/Api'
 
 export const testApi = {
   postTest: (data) => {
-    Api.post(`/lol`, data)
+    Api.post(`/dentistimotest`, data)
   }
 }
