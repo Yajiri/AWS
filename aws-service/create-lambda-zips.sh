90f1eb8ac10af6e8d1186a6cf561552c72aa16d7
Yajiri <90855500+Yajiri@users.noreply.github.com>
2022-12-22 19:30:01 +0100

    Created the scripts for Linux
    scripts created that can be read from any Linux command line.

---
+++ aws-service/create-lambda-zips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [ -d "build" ]
+then
+    rm -rf build
+fi 
+mkdir build
+cd build
+zip DeleteFromDDB.zip ../src/DeleteFromDDB/index.js
+zip DeleteFromEB.zip ../src/DeleteFromEB/index.js
+zip ReadFromDDB.zip ../src/ReadFromDDB/index.js
+zip WriteToDDB.zip ../src/WriteToDDB/index.js
+zip WriteToEB.zip ../src/WriteToEB/index.js
+
+aws s3 cp DeleteFromDDB.zip s3://sentismotest
+aws s3 cp DeleteFromEB.zip s3://sentismotest
+aws s3 cp ReadFromDDB.zip s3://sentismotest
+aws s3 cp WriteToDDB.zip s3://sentismotest
+aws s3 cp WriteToEB.zip s3://sentismotest
