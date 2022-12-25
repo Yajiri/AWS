e62b1b201965fdda025e5286b8d9f0582e5748b3
juliavankirk <juliavankirk@gmail.com>
2022-12-25 21:49:24 +0100

    date and clinic data now get saved to localstorag, will integrate error handling later

--- client/src/Home/Overview.tsx
+++ client/src/Home/Overview.tsx
@@ -7,6 +7,17 @@ const Content1 = styled.div`
   width: 100%;
   height: 80%;
 `;
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  color: "palevioletred";
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 type OverviewProps = {
 name: string
 dentists: number
@@ -16,6 +27,7 @@ city: string
 
 const Home = () => {
   const [data, setClinic] = useState([]);
+  const [selectedDate, setDate] = useState();
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem('clinic') || '{}');
@@ -24,6 +36,20 @@ const Home = () => {
     }
   }, [data])
 
+  useEffect(() => {
+    const selectedDate = JSON.parse(localStorage.getItem('date') || '{}');
+    if (selectedDate) {
+      setClinic(selectedDate);
+    }
+  }, [selectedDate])
+
+  console.log("clinic: " + data + "")
+
+  /* If there is no selected clinic or no selected date, the button should be disabled
+  ** if (!data || !selectedDate)
+  **/
+
+
   return(
     <Content1>
         <h3>Clinic Information</h3>
@@ -31,6 +57,7 @@ const Home = () => {
         <h4>Address: {data[2]}, {data[3]}</h4>
         <h4>Dentists: {data[1]}</h4>        
         <h4>Opening hours:</h4>
+        <Button>Search Times</Button>
     </Content1>
   );
 }
