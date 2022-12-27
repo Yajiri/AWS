bd3c0b6e5f112ac3cd600fb703910d2c355bd137
istrate <istrate@net.chalmers.se>
2022-12-27 01:20:31 +0100

    Convert components to TS and adding them in the new client directory, array map not working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route,Routes, Link } from "react-router-dom";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 import Home from './Home/Home'
 import DatePicker from './DatePicker/DatePicker'
 import Map from './Map/Map'
+import BookAppointment from './Appointments/BookAppointment'
+import BookAppointmentForm from './Appointments/BookAppointmentForm';
 
 const render = (status: Status): ReactElement => {
   if (status === Status.LOADING) return <h3>{status} ..</h3>;
@@ -18,8 +20,11 @@ function App() {
     <div className="App">
       <Router>
       <Wrapper apiKey={'AIzaSyATMY5GYUwukHd29ka5l0DM2R8goauAp0g'} render={render}>
-        <Home />
+        <BookAppointment />
       </Wrapper>
+      <Routes>
+      <Route path='/appointments' element={<BookAppointment />}></Route>
+      </Routes>
       </Router>
       
     </div>
