862b5ab039f8d29408ebdb14558d4666b6efde45
juliavankirk <juliavankirk@gmail.com>
2022-12-28 17:20:40 +0100

    Lambda function that adds a new record when requested booking date and clinic does not yet exist. Lambda function now also successfully retrieves the number of dentists employed within their selected clinic

--- aws-service/src/WriteToDDB/index.js
+++ aws-service/src/WriteToDDB/index.js
@@ -1,15 +1,24 @@
-const AWS = require('aws-sdk');
-const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-central-1'});
-
-exports.handler = function(event, context, callback) {
-    let params = {
-        TableName : "Test",
-        Item: event.detail
-}
- documentClient.put(params, function(err, data){
-         if(err) {
-        callback(err, null);
-    }
-    callback(null, data);
- })
+'use strict'  
+const AWS = require('aws-sdk'); 
+const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-central-1'});  
+exports.handler = function(event, context, callback) {     l
+    let params = {         
+        TableName : "DentistimoBookings",         
+        Item: {             
+            "ClinicId": event.ClinicId,             
+            "Date": event.Date,
+            "TimeSlots": [{
+                "Email": [{
+                    event.email
+                }],
+                "Time": event.time
+            }]                     
+        }     
+    }  
+    documentClient.put(params, function(err, data){          
+        if(err) {         
+            callback(err, null);     
+        }     
+        callback(null, data);  
+    }) 
 }
