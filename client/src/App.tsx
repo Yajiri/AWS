017de0cc984ef73a36a5456e154ef28bf1a4cd80
juliavankirk <juliavankirk@gmail.com>
2022-12-29 20:11:51 +0100

    map is back on, and navbar works w booking page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,7 @@ import { BrowserRouter as Router, Route,Routes, Link } from "react-router-dom";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 import Home from './Home/Home'
-import DatePicker from './DatePicker/DatePicker'
-import Map from './Map/Map'
 import BookAppointment from './Appointments/BookAppointment'
-import BookAppointmentForm from './Appointments/BookAppointmentForm';
 
 const render = (status: Status): ReactElement => {
   if (status === Status.LOADING) return <h3>{status} ..</h3>;
@@ -16,14 +13,16 @@ const render = (status: Status): ReactElement => {
 
 function App() {
   return (
-    
     <div className="App">
       <Router>
-      <Wrapper apiKey={'AIzaSyATMY5GYUwukHd29ka5l0DM2R8goauAp0g'} render={render}>
-        <BookAppointment />
-      </Wrapper>
       <Routes>
-        <Route path='/appointments' element={<BookAppointment />}/>
+        <Route path='/' element={
+          <Wrapper apiKey={'AIzaSyATMY5GYUwukHd29ka5l0DM2R8goauAp0g'} render={render}>
+            <Home />
+          </Wrapper>
+          } 
+        />
+        <Route path='/appointments' element={<BookAppointment />} />
       </Routes>
       </Router>
       
