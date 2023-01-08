d007197fbcd07b47d31d487f516c2724b2407249
juliavankirk <juliavankirk@gmail.com>
2023-01-08 11:56:35 +0100

    check in Ella's types

--- client/src/types/IClinic.ts
+++ client/src/types/IClinic.ts
@@ -1,4 +1,5 @@
 export default interface IClinic {
+    //comment
     city: {
         S: string
     },
--- client/src/types/ITimeSlots.ts
+++ client/src/types/ITimeSlots.ts
@@ -1,4 +1,6 @@
 export default interface ITimeSlots{
+    //comment
+    
     clinicId: string | null,
     date: string | null,
     timeSlots : any
